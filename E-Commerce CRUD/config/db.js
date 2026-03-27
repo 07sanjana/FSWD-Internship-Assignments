04ad26f38337ee71dd7ee2cfec9d195c7506a138
Sanjana M <sanjanamadhu19@gmail.com>
2026-03-27 18:33:21 +0530

    more updates

--- E-Commerce CRUD/config/db.js
+++ E-Commerce CRUD/config/db.js
@@ -4,7 +4,7 @@ dns.setServers(['8.8.8.8', '1.1.1.1']);
 const mongoose = require("mongoose");
 const connectDB = async() => {
     try {
-        await mongoose.connect("mongodb+srv://sanjanamadhu19_db_user:Chintu19@cluster0.zt4jdff.mongodb.net/");
+        // secret in whatsapp
         console.log("MongoDB is successfully connected.");
     } catch (error) {
         console.log(error);
